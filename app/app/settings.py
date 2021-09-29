a2d877899065b059ba1814f7167e50408521fe77
Gigel Babliuc <catanadominic@gmail.com>
2021-09-29 14:59:28 +0300

    staticfiles

--- app/app/settings.py
+++ app/app/settings.py
@@ -121,7 +121,7 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
